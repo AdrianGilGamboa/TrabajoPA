4aad147c9862006ab376b962479ff708a9c4c959
PedroRisquez <pedrorisquez@hotmail.com>
2019-12-18 14:00:02 +0100

    Inicio basedatos 2 #1

---
+++ conexion.php
@@ -0,0 +1,31 @@
+<?php
+
+//SIGLETON
+class baseDatos {
+
+    private static $instance;
+    private $con;
+
+    public static function getInstance() {
+        if (!isset(self::$instance)) {
+            self::$instance = new self();
+        }
+        return self::$instance;
+    }
+
+    private function __construct() {
+        $config = require_once 'baseDatos.php';
+        $this->con = new mysqli(config["host"], config["user"], config["pass"], config["database"]);
+        if ($this->con->connect_errno) {
+            die("Error: " . $this->con->connect_error);
+        }
+        $this->con->set_charset($config["charset"]);
+    }
+    
+    public function getConnection() {
+        return $this->con;
+    }
+
+}
+?>
+
