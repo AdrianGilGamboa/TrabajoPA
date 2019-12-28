d672c6df905f01f09f1b337e7690b49857a675af
PedroRisquez <pedrorisquez@hotmail.com>
2019-12-28 23:05:47 +0100

    -

--- Secciones.php
+++ Secciones.php
@@ -1,11 +1,11 @@
 <!DOCTYPE html>
 <?php
-    require_once 'DAOGenerico.php';
-    require_once 'DAOSeccion.php';
-    require_once 'Seccion.php';
- $DAOSeccion=new DAOSeccion();
- $seccion1 = new Seccion(null,'futbol');
- DAOSeccion::readAll();
+require_once 'DAOGenerico.php';
+require_once 'DAOSeccion.php';
+require_once 'Seccion.php';
+$DAOSeccion = new DAOSeccion();
+$seccion1 = new Seccion(null, 'futbol');
+$DAOSeccion::readAll();
 ?>
 <html>
     <head>
@@ -13,6 +13,6 @@
         <title></title>
     </head>
     <body>
-        
+
     </body>
 </html>
