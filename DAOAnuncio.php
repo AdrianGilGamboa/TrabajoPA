13b7ae871a6bd3dc04aa620da94d769697f99e1c
paulamg24 <paulamelgar24@gmail.com>
2019-12-28 13:48:59 +0100

    actualización Paula DAOAnuncio #1

--- DAOAnuncio.php
+++ DAOAnuncio.php
@@ -1,21 +1,57 @@
 <?php
+
 require_once 'DaoGenerico.php';
 
 use DaoGenerico;
 
-class DAOCuenta extends DAOGenerico{
-    
+class DAOCuenta extends DAOGenerico {
+
     public function __construct() {
-        parent::__construct("cuenta");
+        parent::__construct("anuncios");
     }
 
+    public function create($anuncioDatos) {
+        $descripcion = $anuncioDatos['descripcion'];
+        $imagen = $anuncioDatos['imagen'];
+        $duracion = $anuncioDatos['duracion'];
+        $resul = False;
+        $query = "INSERT INTO anuncios (descripcion,imagen, duracion) VALUES ('$descripcion','$imagen','$duracion')";
+        $result = $this->con->query($query);
+        if ($result) {
+            $resul = True;
+        }
+        return $resul;
+    }
+
+    public function update($anuncioDatos) {
+        $descripcion = $anuncioDatos['descripcion'];
+        $imagen = $anuncioDatos['imagen'];
+        $duracion = $anuncioDatos['duracion'];
+        $id = $anuncioDatos['id'];
+        $resul = False;
+        $query = "UPDATE anuncios SET descripcion='$descripcion', imagen='$imagen' , duracion='$duracion' WHERE id=$id";
+        $result = $this->con->query($query);
+        if ($result) {
+            $resul = True;
+           
+        }
+        return $resul;
+    }
     
-    public function create($obj) {
-        
+    public function delete($id): boolean {
+        return parent::delete($id);
+    }
+    public function buscar($columna, $valor) {
+        return parent::buscar($columna, $valor);
     }
 
-    public function update($obj) {
-        
+    public function read($id) {
+        return parent::read($id);
     }
+
+    public function readAll(): array {
+        return parent::readAll();
+    }
+
 
 }
