af7b91ca5163309d6c37b9570c952dcdb5c6621b
PedroRisquez <pedrorisquez@hotmail.com>
2020-01-17 11:09:27 +0100

    Registro e inicio de sesion #10

--- vistas/portada.php
+++ vistas/portada.php
@@ -11,17 +11,32 @@ and open the template in the editor.
     </head>
     <body>
         <?php
-        // put your code here
+        session_start();
+        $hayCuenta = FALSE;
+        if (isset($_SESSION['cuentaID'])) {
+            $hayCuenta = TRUE;
+            $nombreUsuario = $_SESSION['nombreUsuario'];
+            $idUsuario = $_SESSION['cuentaID'];
+        }
         ?>
         <header>
             <a href="inicioSesion.php">Inciar Sesion</a>
+            <a href="registro.php">Registrarse</a>
+            <?php
+            if ($hayCuenta) {
+                ?>
+                <a href="cuenta.php"><?php echo $nombreUsuario;?></a>
+                <?php
+            }
+            ?>
+
         </header>
         <nav>
-            
+
         </nav>
         <footer>
-            
+
         </footer>
-        
+
     </body>
 </html>
